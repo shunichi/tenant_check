842b8ec901be5f4d782fe3f076c6c06c33f4657e
Shunichi Ikegami <sike.tm@gmail.com>
2018-05-20 02:16:28 +0900

    Add test for github rails master

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-BUNDLE_GEMFILE=Gemfile.rails-5.2 bundle update && BUNDLE_GEMFILE=Gemfile.rails-5.2 bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.rails-5.1 bundle update && BUNDLE_GEMFILE=Gemfile.rails-5.1 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.rails-5.1 bundle install && BUNDLE_GEMFILE=Gemfile.rails-5.1 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.rails-5.2 bundle install && BUNDLE_GEMFILE=Gemfile.rails-5.2 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.rails-master bundle install && BUNDLE_GEMFILE=Gemfile.rails-master bundle exec rspec spec
