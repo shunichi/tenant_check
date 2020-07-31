ed94709d30c2a291115c2deb6310910db067c7eb
Shunichi Ikegami <sike.tm@gmail.com>
2020-08-01 06:45:17 +0900

    Set up GitHub actions

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 
 BUNDLE_GEMFILE=Gemfile.rails-5.1 bundle install && BUNDLE_GEMFILE=Gemfile.rails-5.1 bundle exec rspec spec
 BUNDLE_GEMFILE=Gemfile.rails-5.2 bundle install && BUNDLE_GEMFILE=Gemfile.rails-5.2 bundle exec rspec spec
-BUNDLE_GEMFILE=Gemfile.rails-6 bundle install && BUNDLE_GEMFILE=Gemfile.rails-6 bundle exec rspec spec
+BUNDLE_GEMFILE=Gemfile.rails-6.0 bundle install && BUNDLE_GEMFILE=Gemfile.rails-6.0 bundle exec rspec spec
 BUNDLE_GEMFILE=Gemfile.rails-master bundle install && BUNDLE_GEMFILE=Gemfile.rails-master bundle exec rspec spec
